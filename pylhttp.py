84d4babb8d9fbe7a1f7acde6a8267f398d6da17f
Anton Gorunov <anton.gorunov@gmail.com>
2012-10-03 19:39:06 +0300

    awesome docs and examples added

--- pylhttp.py
+++ pylhttp.py
@@ -59,7 +59,7 @@ class BindableHTTPHandler(urllib2.HTTPHandler):
             self.customip = socket.gethostbyname_ex(socket.gethostname())[2][0]
         else:
             self.customip = ip
-    
+
     def http_open(self, req):
         return self.do_open(BindableHTTPConnectionFactory(self.customip), req)
 
@@ -129,7 +129,7 @@ class PylHttpResponse(object):
             self.processErrorResponse()
 
         self.dict = self.__dict__
-        
+
 
     def processErrorResponse(self):
         reason = self.error.__dict__.get('reason', '')
@@ -147,11 +147,11 @@ class PylHttpResponse(object):
                 self.error_message = 'timeout'
             else:
                 self.error_message = str(reason)
-                
+
         else:
             self.headers = self.error.headers.dict
             """ Error response processing """
-            
+
             #print 'Error code: ', e.code
             """ Handling gzip response
                 And retriewing page content """
@@ -178,12 +178,12 @@ class PylHttpResponse(object):
             self.charset = ''
             self.error_message = str(self.error.code)
 
-    
+
     def processCorrectResponse(self):
 
         self.headers = self.response.headers.dict
         self.error_message = ''
-        
+
         """ Handling gzip response
             And retriewing page content """
         try:
@@ -193,7 +193,7 @@ class PylHttpResponse(object):
                 self.content = gzipper.read()
         except KeyError:
             self.content = self.response.read()
-        
+
 
         """ Let's get HTTP response status code """
         try:
@@ -233,7 +233,7 @@ class Client(object):
 
         self.savehistory = savehistory
         self.history = []
-        self.timeout = timeout        
+        self.timeout = timeout
         self.user_agent = user_agent
         self.cookiejar = cookielib.CookieJar()
         self.smartRedirectHandler = SmartRedirectHandler()
@@ -248,7 +248,7 @@ class Client(object):
             self.proxyHandler
         )
 
-    
+
     def get_user_agent(self):
         user_agents = (
             'Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.10) Gecko/20100914 Firefox/3.6.10',
@@ -256,7 +256,7 @@ class Client(object):
         )
         return random.choice(user_agents)
 
-    
+
     def makeproxy(self, proxystring, proxytype='http'):
         """
         Splits default proxy as 255.255.255.255:3128 into urllib format
@@ -265,14 +265,14 @@ class Client(object):
             ip, port = proxystring.split(':')
         except AttributeError:
             ip, port = None, None
-        
+
         if ip and port:
             proxy = {proxytype: 'http://{ip}:{port}/'.format(ip=ip, port=port)}
         else:
             proxy = None
         return proxy
 
-    
+
     def request(self, url, params=None, timeout=None, proxy=None, referer=None, headers=None):
         """ Main request function
             Returns html or empty content """
@@ -298,21 +298,21 @@ class Client(object):
         request.add_header('Accept-encoding', 'gzip')
         for h in headers:
             request.add_header(h[0], h[1])
-        
+
         if referer == 'auto' and self.history:
             request.add_header('Referer', self.history[-1]['response'].realurl)
         elif referer:
             request.add_header('Referer', referer)
-            
+
         request_time = time.time()
-       
+
         try:
             """ Trying to open URL """
             response = self.opener.open(request, timeout=current_timeout)
-            self.response = PylHttpResponse(url=url, request_time=request_time, 
+            self.response = PylHttpResponse(url=url, request_time=request_time,
                             redirect_handler=self.smartRedirectHandler, response=response)
         except urllib2.URLError as e:
-            self.response = PylHttpResponse(url=url, request_time=request_time, 
+            self.response = PylHttpResponse(url=url, request_time=request_time,
                             redirect_handler=self.smartRedirectHandler, error=e)
         # Memorize browsing data
         if self.savehistory:
@@ -321,55 +321,114 @@ class Client(object):
 
 
 if __name__ == "__main__":
-    # 1. Simplicity! All hard thing done by me :-)
+    # FOA sorry for my English - I'm not native speaker!
+
+    # All hard thing done by me :-) Now your bots may be happy.
+
     html = Client().request('http://www.google.com.ua/').content
     print html
-    # You can get all properties of response by this call:
-    response_object = Client().request('http://www.google.com.ua/')
+
+    # Or you can do something like that:
+    bot = Client(savehistory=True)
+    response = bot.request('http://google.com')
+    print response.url
+    print response.status
+    print response.realurl
+    print response.rucode
+    # You see - we following redirects and save all information about source and dest URL
+
+    # Let's make some requests on random sites
+    bot.request('http://atape.net/')
+    bot.request('http://github.com/')
+    bot.request('http://yahoo.com/')
+
+    # When you travelling the web with Client object, you have cookies and browsing history if last enabled
+    print bot.cookiejar
+
+    # Note that history is simple list, that contains Request and Response objects for each webpage visited.
+    print bot.history
+
+    # If you want to get headers of last visited page do the following:
+    print bot.history[-1]['response'].headers
+    # Or if you want to remember first requested(visited) url:
+    print bot.history[0]['response'].url
+
+    # Request object is standard urllib2.Request - I don't know why you need it :-)
+    print isinstance(bot.history[0]['request'], urllib2.Request)
+
+
+    # You can initiate any number of separate clients. It can be useful, for example for bots under proxies
+    #bot1 = Client(user_agent='BOT1', timeout=10, proxy='100.255.255.1:3128', savehistory=True)
+    #bot2 = Client(user_agent='BOT2', timeout=30, proxy='200.255.255.2:3128', savehistory=True)
+    #bot3 = Client(user_agent='BOT3', timeout=60, proxy='300.255.255.3:3128', savehistory=True)
+    # Now you can browse the web via 3 different clients.
+
+    # You can get all properties of response object by this call:
     # Client().request method returns PylHttpResponse object
+    response_object = Client().request('http://www.google.com.ua/')
     print response_object.dict.keys()
-    '''
+    ''' Here short description
     [
-        'url',               - requested url 
+        'url',               - requested url
         'status',            - status code of your url -> 200/404/301/302, anything else
         'realurl',           - redirected url
         'rucode',            - realurl status code -> 200/404/301/302, anything else
-        'charset',           - charset from headers 
+        'charset',           - charset from headers
         'error_message',     - your custom error message. 'timeout' for example
         'content',           - html content decoded to unicode
         'headers',           - dictionary of response headers
         'duration',          - page load duration
         'type',              - content type of the page from headers
         'response',          - response object from urllib2
-        'error',             - error object from urllib2 (URLError instance) 
+        'error',             - error object from urllib2 (URLError instance)
         'size'               - content size calculated in symbols
-        'dict',              - all this properties in dictionary-like style 
+        'dict',              - all this properties in dictionary-like style
     ]
     '''
 
-    #2. GET and POST requests support
-    #3. Redirect handler and memorizer
-    #4. GZIP support implemented -> faster way to get your webpages
-    #5. Cookie processing in current session
-    #6. Custom headers
-    #7. Proxy or custom IP interface
-    #8. Browsing history and Referer if needed (download a lot of pages without extra code)
-    #9. Usable Response information
-    #10. Can be asynchonous easy with gevent monkey patch. Or you can use it with threads.
+    # POST requests easy as piece of cake ->
+    url = 'http://www.google.com/'
+    post = {
+        'var1': 'value1',
+        'var2': 'value2'
+    }
+    bot.request(url, params=post)
+
+    # You can add any custom headers to your request
+    headers = [('header1', 'value'), ('header2', 'value')]
+    bot.request(url, params=post, headers=headers)
 
 
-    '''
-    import gevent 
+    # If you have not proxies, but your server have custom IP interfaces
+    # you can specify just IP address
+    # bot = Client(ip_address='111.255.255.255')
+
+    # Some times it is important to provide referer information in headers
+    # you can specify this referer manually or use 'auto' keyword - it will be last visited url
+    bot.request(url, referer='auto')
+
+
+    # and.... this awesome thing can be asynchonous with gevent monkey patch.
+
+    import gevent
     import gevent.monkey
     gevent.monkey.patch_socket()
-    
-    bot = Client()
+
+    print "start gevent threads"
+    multibot = Client(savehistory=True)
     threads = [
-        gevent.spawn(bot.request, 'http://www.google.com.ua/'),
-        gevent.spawn(bot.request, 'http://www.google.com/'),
-        gevent.spawn(bot.request, 'http://google.com.ua/'),
+        gevent.spawn(multibot.request, 'http://www.google.com.ua/'),
+        gevent.spawn(multibot.request, 'http://test.com/'),
+        gevent.spawn(multibot.request, 'http://facebook.com/'),
+        gevent.spawn(multibot.request, 'http://google.com/'),
+        gevent.spawn(multibot.request, 'http://google.ru/'),
+        gevent.spawn(multibot.request, 'http://yahoo.com/'),
+        gevent.spawn(multibot.request, 'http://bing.com/'),
+        gevent.spawn(multibot.request, 'http://i.ua/'),
     ]
     gevent.joinall(threads)
-    print bot.history
-    '''
+    for d in multibot.history:
+        response = d['response']
+        print "%s : %s --> %s : %s === %s %s" % (response.status, response.url, response.rucode,
+                            response.realurl,  response.content[:15]+"...", response.duration)
 
